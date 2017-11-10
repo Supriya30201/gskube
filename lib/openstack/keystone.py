41ec749507fcbc3f4ccffd43eea6cda811be076a
Payal Rahatal <GS-1433@GSLAB.COM>
2017-11-10 09:52:28 +0530

    Exception Handling in keystone part

--- lib/openstack/keystone.py
+++ lib/openstack/keystone.py
@@ -17,7 +17,7 @@ def unscoped_login(protocol, host, port, username, password):
         user_id = client.session.get_user_id(auth)
         return {'token': token, 'client': client, 'user_id': user_id}
     except Exception as e:
-        raise OpenstackException("Exception while performing unscoped login : " + e.message, e)
+        raise OpenstackException(message="Exception while performing unscoped login : " + e.message, exception=e)
 
 
 # Uses:For User operations(create/delete/list users) and compute operations(create/delete/list/start/stop instance)
@@ -29,7 +29,7 @@ def scoped_login_v3(protocol, host, port, token, project_id):
         connection = v3_client.Client(session=session)
         return {'client': connection, 'session': session}
     except Exception as e:
-        raise OpenstackException(e.message, e)
+        raise OpenstackException("Exception while performing scoped login : " + e.message, exception=e)
 
 
 # List projects using v2 unscoped token
@@ -38,7 +38,7 @@ def list_projects(client):
         projects_list = client.tenants.list()
         return projects_list
     except Exception as e:
-        raise OpenstackException("Exception while getting list of projects : " + e.message, e)
+        raise OpenstackException(message="Exception while getting list of projects : " + e.message, exception=e)
 
 
 def get_user_roles(client, user_id, project_id):
@@ -50,7 +50,8 @@ def get_user_roles(client, user_id, project_id):
             user_roles.append(role.name)
         return user_roles
     except Exception as e:
-        raise OpenstackException(e.message, e)
+        raise OpenstackException(message="Exception while getting roles for specified project and user : " + e.message,
+                                 exception=e)
 
 
 def is_admin(client, user_id, project_id):
@@ -69,7 +70,7 @@ def create_user(client, name, password, email, description, roles=None, project_
             assign_roles(client, new_user.id, roles, project_id)
         return new_user.id
     except Exception as e:
-        raise OpenstackException(message=e.message, exception=e)
+        raise OpenstackException(message="Exception while creating user in openstack : " + e.message, exception=e)
 
 
 def get_roles(client):
@@ -77,7 +78,7 @@ def get_roles(client):
         roles = client.roles.list()
         return roles
     except Exception as e:
-        raise OpenstackException(e)
+        raise OpenstackException(message="Exception while getting roles from openstack: " + e.message, exception=e)
 
 
 def assign_roles(client, user_id, roles, project_id):
@@ -86,7 +87,7 @@ def assign_roles(client, user_id, roles, project_id):
             client.roles.grant(role=role, user=user_id, project=project_id)
 
     except Exception as e:
-        raise OpenstackException(e.message, e)
+        raise OpenstackException(message="Exception while assiging roles: " + e.message, exception=e)
 
 
 def remove_user_role(client, user_id, roles, project_id):
@@ -94,4 +95,4 @@ def remove_user_role(client, user_id, roles, project_id):
         for role in roles:
             client.roles.revoke(role=role, user=user_id, project=project_id)
     except Exception as e:
-        raise OpenstackException(e)
+        raise OpenstackException(message="Exception while removing roles of user: " + e.message, exception=e)
