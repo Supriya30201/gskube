d8a01c0f61238518f42a878023c10f16d3015f2c
Sanket Modi <sanket.modi@gslab.com>
2017-12-08 20:23:40 +0530

    change environment variable name

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i "s/'HOST': 'localhost',/'HOST': '$DB_HOST',/g" /var/www/html/ServiceOnline/service_online/settings.py
-sed -i "s/'USER': 'root',/'USER': '$DB_PORT',/g" /var/www/html/ServiceOnline/service_online/settings.py
+sed -i "s/'USER': 'root',/'USER': '$DB_USER',/g" /var/www/html/ServiceOnline/service_online/settings.py
 sed -i "s/'PASSWORD': 'root',/'PASSWORD': '$DB_PASSWORD',/g" /var/www/html/ServiceOnline/service_online/settings.py
 sed -i 's#LOG_FILE_DIR = "logs/"#LOG_FILE_DIR = "/var/www/html/ServiceOnline/logs/"#g' /var/www/html/ServiceOnline/core/log_configuration.py
 sudo chmod u+x /var/www/html/ServiceOnline/service_online/wsgi.py
