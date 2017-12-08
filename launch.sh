a1d5eb0b3470e7f5d04c38cb0b5b79d784f03820
Sanket Modi <sanket.modi@gslab.com>
2017-12-08 19:05:57 +0530

    Updated script as per exceptions

--- launch.sh
+++ launch.sh
@@ -3,7 +3,9 @@ sed -i "s/'HOST': 'localhost',/'HOST': '$DB_HOST',/g" /var/www/html/ServiceOnlin
 sed -i "s/'USER': 'root',/'USER': '$DB_PORT',/g" /var/www/html/ServiceOnline/service_online/settings.py
 sed -i "s/'PASSWORD': 'root',/'PASSWORD': '$DB_PASSWORD',/g" /var/www/html/ServiceOnline/service_online/settings.py
 sed -i 's#LOG_FILE_DIR = "logs/"#LOG_FILE_DIR = "/var/www/html/ServiceOnline/logs/"#g' /var/www/html/ServiceOnline/core/log_configuration.py
-
+sudo chmod u+x /var/www/html/ServiceOnline/service_online/wsgi.py
+mkdir /var/www/html/ServiceOnline/logs
+chmod 777 -R /var/www/html/ServiceOnline/logs
 apachectl stop
 apachectl start
 tail -f /dev/null
