d28af6d6511d7eb49d1cb198fd2e88d8077de4aa
Sanket Modi <sanket.modi@gslab.com>
2017-12-08 17:43:39 +0530

    cloned code using launch file

--- launch.sh
+++ launch.sh
@@ -2,6 +2,8 @@
 sed -i "s/'HOST': 'localhost',/'HOST': '$DB_HOST',/g" /var/www/html/ServiceOnline/service_online/settings.py
 sed -i "s/'USER': 'root',/'USER': '$DB_PORT',/g" /var/www/html/ServiceOnline/service_online/settings.py
 sed -i "s/'PASSWORD': 'root',/'PASSWORD': '$DB_PASSWORD',/g" /var/www/html/ServiceOnline/service_online/settings.py
+git clone http://$GIT_USERNAME:$GIT_PASSWORD@gitlab.gslab.com/sanket.modi/ServiceOnline.git
+cp -r ServiceOnline /var/www/html/
 apachectl stop
 apachectl start
 if [ $# -gt 0 ]; then
