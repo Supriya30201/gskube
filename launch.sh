005ad14a7a9133fcd90a2129d30f42db0454bece
Sanket Modi <sanket.modi@gslab.com>
2017-12-08 17:59:25 +0530

    change code to run container for longer time

--- launch.sh
+++ launch.sh
@@ -4,6 +4,4 @@ sed -i "s/'USER': 'root',/'USER': '$DB_PORT',/g" /var/www/html/ServiceOnline/ser
 sed -i "s/'PASSWORD': 'root',/'PASSWORD': '$DB_PASSWORD',/g" /var/www/html/ServiceOnline/service_online/settings.py
 apachectl stop
 apachectl start
-if [ $# -gt 0 ]; then
-  exec "$@"
-fi
+tail -f /dev/null
